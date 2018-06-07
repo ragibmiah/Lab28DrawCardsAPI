960a6c77c1689f398ba0b69f01f53e5d795a533d
Ragib Miah <ragib@umich.edu>
2018-06-07 09:02:55 -0400

    first commit

---
+++ Lab28CardGame/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab28CardGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HttpWebRequest WR = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/draw/?count=5");
+            WR.UserAgent = ".NET Framework Test Client";
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string Card = reader.ReadToEnd();
+
+            try
+            {
+                JObject JsonData = JObject.Parse(Card);
+                ViewBag.Cards = JsonData["cards"];
+                ViewBag.deckID = JsonData["deck_id"];
+                //ViewBag.Image = JsonData["cards"]["image"];
+                //ViewBag.Value = JsonData["cards"]["value"];
+                //ViewBag.Suit = JsonData["cards"]["suit"];
+                ViewBag.Remaining = JsonData["remaining"];
+
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
